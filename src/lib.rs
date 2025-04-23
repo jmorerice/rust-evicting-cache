41b2e101016ecab3da1e57f1c714a94291a0b9cb
Joshua Moraes <jmorerice@gmail.com>
2025-04-22 23:24:26 -0700

    started LRU policy

--- src/lib.rs
+++ src/lib.rs
@@ -3,29 +3,52 @@
 
 use std::collections::HashMap;
 
-pub struct Cache<K, V> {
-    capacity: usize,
-    store: HashMap<K, V>,
+// Trait for pluggable eviction policies
+pub trait EvictionPolicy<K> {
+    // Called on every key access (for LRU/LFU updates)
+    fn record_access(&mut self, key: &K);
+    fn record_insertion(&mut self, key: &K);
+    fn record_removal(&mut self, key: &K);
+    fn evict_candidate(&mut self) -> Option<K>;
 }
 
-impl<K: std::cmp::Eq + std::hash::Hash, V> Cache<K, V> {
-    pub fn new(capacity: usize) -> Self {
+pub struct Cache<K, V, P> 
+where
+    K: std::cmp::Eq + std::hash::Hash + Clone,
+    P: EvictionPolicy<K>,
+{
+    capacity: usize,
+    store: HashMap<K, V>,
+    policy: P
+}
+
+impl<K, V, P> Cache<K, V, P> 
+where 
+    K: std::cmp::Eq + std::hash::Hash + Clone,
+    P: EvictionPolicy<K>,
+{
+    pub fn new(capacity: usize, policy: P) -> Self {
         Cache {
             capacity,
             store: HashMap::new(),
+            policy,
         }
     }
 
     pub fn get(&mut self, key: &K) -> Option<&V> {
+        self.policy.record_access(key); //Track recency
         self.store.get(key)
-        // TODO: with LRU this will need to update usage stats
     }
 
     pub fn put(&mut self, key: K, value: V) {
-        if self.store.len() >= self.capacity {
-            // TODO: implement eviction policy (e.g., FIFO, LRU, LFU)
+        if self.store.len() >= self.capacity && !self.store.contains_key(&key) {
+            if let Some(evict_key) = self.policy.evict_candidate() {
+                self.store.remove(&evict_key);
+                self.policy.record_removal(&evict_key);
+            }
         }
 
-        self.store.insert(key, value);
+        self.store.insert(key.clone(), value);
+        self.policy.record_insertion(&key)
     }
 }
